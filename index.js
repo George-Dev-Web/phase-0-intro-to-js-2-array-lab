d1ee8c33fc00f94ab7914b6ae4ee753bdb45c36a
George <georgemwazuna3@gmail.com>
2025-03-13 11:48:50 +0300

    Array 1

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+// Destructive functions
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+// Non-destructive functions
+function appendCat(name) {
+  return [...cats, name]; // Returns a new array with the cat added at the end
+}
+
+function prependCat(name) {
+  return [name, ...cats]; // Returns a new array with the cat added at the beginning
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1); // Returns a new array without the last cat
+}
+
+function removeFirstCat() {
+  return cats.slice(1); // Returns a new array without the first cat
+}
